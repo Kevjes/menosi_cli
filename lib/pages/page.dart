85625f4af4ee14551eb23fcd1081c6df1b585c31
kevin <kevinjessi10@gmail.com>
2024-08-21 11:13:28 +0100

    update

--- lib/pages/page.dart
+++ lib/pages/page.dart
@@ -15,9 +15,9 @@ void createPage(String featureName, String pageName) {
   final bindingPath = p.join(
       currentDir, 'lib', 'features', featureName, 'navigation', 'bindings');
   final navigationFilePath = p.join(currentDir, 'lib', 'features', featureName,
-      'navigation', '${featureName}_navigation.dart');
+      'navigation', 'private', '${featureName}_private_navigation.dart');
   final routesFilePath = p.join(currentDir, 'lib', 'features', featureName,
-      'navigation', '${featureName}_routes.dart');
+      'navigation', 'private', '${featureName}_private_routes.dart');
 
   // Create the directory structure
   Directory(pagePath).createSync(recursive: true);
@@ -27,9 +27,11 @@ void createPage(String featureName, String pageName) {
   // Create basic files with templates
   File(p.join(pagePath, '${convertToSnakeCase(pageName)}_screen.dart'))
       .writeAsStringSync(pageTemplate(pageName));
-  File(p.join(controllerPath, '${convertToSnakeCase(pageName)}_controller.dart'))
+  File(p.join(
+          controllerPath, '${convertToSnakeCase(pageName)}_controller.dart'))
       .writeAsStringSync(controllerTemplate(pageName));
-  File(p.join(bindingPath, '${convertToSnakeCase(pageName)}_controller_binding.dart'))
+  File(p.join(bindingPath,
+          '${convertToSnakeCase(pageName)}_controller_binding.dart'))
       .writeAsStringSync(bindingTemplate(pageName));
 
   // Update routes file
@@ -37,18 +39,17 @@ void createPage(String featureName, String pageName) {
   final routesFileContent = routesFile.readAsStringSync();
   final updatedRoutesContent = routesFileContent.replaceFirst(
       '// Add other routes here',
-      "static const String ${pageName.toUpperCase()} = '\$_prefix/${pageName}';\n  // Add other routes here");
+      "static const String $pageName = '\$home/$pageName';\n  // Add other routes here");
 
   routesFile.writeAsStringSync(updatedRoutesContent);
 
   // Update navigation file
   final navigationFile = File(navigationFilePath);
   final navigationFileContent = navigationFile.readAsStringSync();
-  final updatedNavigationContent = navigationFileContent.replaceFirst(';', """;\n
-import 'bindings/${convertToSnakeCase(pageName)}_controller_binding.dart';
-import '../presentation/${pageName}/${convertToSnakeCase(pageName)}_screen.dart';""").replaceFirst(
-      '// Add other routes here',
-      "GetPage(\n      name: ${capitalize(featureName)}Routes.${pageName.toLowerCase()},\n      page: () => ${capitalize(pageName)}Screen(),\n      binding: ${capitalize(pageName)}ControllerBinding(),\n    ),\n    // Add other routes here");
+  final updatedNavigationContent =
+      navigationFileContent.replaceFirst(';', """;\n
+import '../bindings/${convertToSnakeCase(pageName)}_controller_binding.dart';
+import '../../presentation/$pageName/${convertToSnakeCase(pageName)}_screen.dart';""").replaceFirst('// Add other routes here', "GetPage(\n      name: ${capitalize(featureName)}PrivateRoutes.${(pageName)},\n      page: () => ${capitalize(pageName)}Screen(),\n      binding: ${capitalize(pageName)}ControllerBinding(),\n    ),\n    // Add other routes here");
   navigationFile.writeAsStringSync(updatedNavigationContent);
 
   print('Page "$pageName" created successfully within feature "$featureName".');
